1895ead0058509c7db693576a9138b837b40d0f1
Samuel Mimram <smimram@gmail.com>
2019-06-21 00:13:25 +0200

    Correct types.

--- src/type.ml
+++ src/type.ml
@@ -13,18 +13,18 @@ type t =
    | Int
    | Float
    | String
-   | UVar of uvar
-   | EVar of t option ref
+   | UVar of t option ref
+   | EVar of evar
    (** An existential type variable: this is an opaque type whose contents will
       be revealed later (it is used only for states at the moment because they
       are not known at typing time). They cannot be substituted by types with
       universal variables. *)
    | Arr of t * t
    | Record of (string * t) list
- and uvar = uvar_contents ref
+ and evar = evar_contents ref
  (** Contents of a variable. *)
- and uvar_contents =
-   | FVar of int ref (** A free variable at given level. *)
+ and evar_contents =
+   | Level of int (** A free variable at given level. *)
    | Link of t (** A link to another type. *)
 
 let make t = { desc = t }
@@ -39,17 +39,11 @@ let string () = make String
 
 let record l = make (Record l)
 
-let invar =
-  let n = ref 0 in
-  fun level ->
-    incr n;
-    ref (FVar (ref level))
-                  
-let uvar level =
-  make (UVar (invar level))
+let uvar () =
+  make (UVar (ref None))
 
-let evar () =
-  make (EVar (ref None))
+let evar level =
+  make (EVar (ref (Level level)))
 
 let arr a b = make (Arr (a, b))
 
@@ -57,8 +51,8 @@ let arr a b = make (Arr (a, b))
 let unvar t =
   let rec aux t =
     match t.desc with
-    | UVar { contents = Link t } -> aux t
-    | EVar { contents = Some t } -> aux t
+    | UVar { contents = Some t } -> aux t
+    | EVar { contents = Link t } -> aux t
     | _ -> t
   in
   aux t
@@ -74,18 +68,18 @@ let to_string t =
     | UVar v ->
        (
          match !v with
-         | Link t ->
+         | Some t ->
             if !Config.Debug.Typing.show_links
             then Printf.sprintf "[%s]" (to_string false t)
             else to_string false t
-         | FVar level ->
-            un v ^ (if !Config.Debug.Typing.show_levels then "@" ^ string_of_int !level else "")
+         | None -> un v
        )
     | EVar v ->
        (
          match !v with
-         | Some t -> Printf.sprintf "?[%s]" (to_string false t)
-         | None -> "?" ^ en v
+         | Link t -> Printf.sprintf "?[%s]" (to_string false t)
+         | Level l ->
+            "?" ^ en v ^ (if !Config.Debug.Typing.show_levels then "@" ^ string_of_int l else "")
        )
     | Int -> "int"
     | Float -> "float"
@@ -137,22 +131,27 @@ end
 let rec occurs x t =
   match (unvar t).desc with
   | Arr (a, b) -> occurs x a || occurs x b
-  | UVar v -> x == v
-  | EVar _ -> false
+  | EVar v -> x == v
+  | UVar _ -> false
   | Int | Float | String | Bool -> false
   | Record l -> List.exists (fun (_,t) -> occurs x t) l
 
 let update_level l t =
   let rec aux t =
-    let update_var v =
-      match !v with
-      | Link t -> aux t
-      | FVar l' -> l' := min l !l'
-    in
     match t.desc with
     | Arr (a, t) -> aux t
-    | UVar v -> update_var v
-    | EVar v -> (match !v with Some t -> aux t | None -> ())
+    | UVar v ->
+       (
+         match !v with
+         | Some t -> aux t
+         | None -> ()
+       )
+    | EVar v ->
+       (
+         match !v with
+         | Link t -> aux t
+         | Level l' -> v := Level (min l l')
+       )
     | Int | Float | String | Bool -> ()
     | Record l -> List.iter (fun (_,t) -> aux t) l
   in
@@ -164,17 +163,17 @@ let rec ( <: ) (t1:t) (t2:t) =
     let t2 = unvar t2 in
     match t1.desc, t2.desc with
     | UVar v1, UVar v2 when v1 == v2 -> true
-    | _, UVar ({ contents = FVar l } as v2) ->
-       if occurs v2 t1 then false
-       else
-         (
-           update_level !l t1;
-           if !Config.Debug.Typing.show_assignations then Printf.printf "%s <- %s\n%!" (to_string t2) (to_string t1);
-           v2 := Link t1;
-           true
-         )
     | UVar _, _ -> t2 <: t1
     | EVar v1, EVar v2 when v1 == v2 -> true
+    | EVar ({ contents = Level l } as x), _ ->
+       if occurs x t2 then false
+       else
+         (
+           update_level l t2;
+           if !Config.Debug.Typing.show_assignations then Printf.printf "%s <- %s\n%!" (to_string t1) (to_string t2);
+           x := Link t2;
+           true
+         )
     | Arr (a, b), Arr (a', b') ->
        a' <: a && b <: b'
     | Record l1, Record l2 ->
@@ -185,28 +184,29 @@ let rec ( <: ) (t1:t) (t2:t) =
     | String, String -> true
     | _, _ -> false
 
+(** Generalize existential variable to universal ones. *)
 let rec generalize level t =
   let desc =
     match (unvar t).desc with
     | UVar v -> UVar v
-    | EVar v -> 
+    | EVar { contents = Level l } as t -> if l <= level then t else (uvar ()).desc
+    | EVar { contents = Link _ } -> assert false
     | Arr (a, b) ->
-       let a = List.concat_map (fun (l,(t,o)) -> aux t) a in
-       (aux t)@a
-    | Record l -> List.concat_map (fun (x,t) -> aux t) l
-    | Bool | Int | Float | String -> []
+       Arr (generalize level a, generalize level b)
+    | Record l -> Record (List.map (fun (x,t) -> x , generalize level t) l)
+    | Bool | Int | Float | String as t -> t
   in
   { desc }
 
 (** Instantiate a type scheme: replace universally quantified variables with
     fresh variables. *)
-let instantiate t =
+let instantiate level t =
   let tenv = ref [] in
   let rec aux t =
     let desc =
       match (unvar t).desc with
       | UVar x ->
-         if not (List.mem_assq x !tenv) then tenv := (x, (evar ()).desc) :: !tenv;
+         if not (List.mem_assq x !tenv) then tenv := (x, (evar level).desc) :: !tenv;
          List.assq x !tenv
       | EVar v -> EVar v
       | Arr (a, b) -> Arr (aux a, aux b)
